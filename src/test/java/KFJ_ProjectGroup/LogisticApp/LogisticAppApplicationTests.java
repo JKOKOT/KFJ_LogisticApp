42128d74ed92d2a2f907dd5248b1282dbd02bfbe
jacekkokot <jacekkokot.ue@gmail.com>
2020-11-07 13:27:51 +0100

    first commit

---
+++ src/test/java/KFJ_ProjectGroup/LogisticApp/LogisticAppApplicationTests.java
@@ -0,0 +1,13 @@
+package KFJ_ProjectGroup.LogisticApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
